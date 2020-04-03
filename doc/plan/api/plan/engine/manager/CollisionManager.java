2972a674190184d7d92b0d75dbc8c41f6b06c094
Grant LoPresti <gjl13@duke.edu>
2020-04-03 01:14:45 -0400

    progress on classes

--- doc/plan/api/plan/engine/manager/CollisionManager.java
+++ doc/plan/api/plan/engine/manager/CollisionManager.java
@@ -2,4 +2,11 @@ package plan.engine.manager;
 
 public class CollisionManager {
 
+  public CollisionManager() {
+
+  }
+
+  private void detectCollisions(){};
+
+  private void sendCollisions(){};
 }
