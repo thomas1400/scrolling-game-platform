fd5c4c8e9c6e994e73fede03c40902920ddda530
Thomas Owens <to57@duke.edu>
2020-04-02 23:05:23 -0400

    Moved CollisionManager

--- src/ooga/engine/manager/CollisionManager.java
+++ src/ooga/engine/manager/CollisionManager.java
@@ -1,5 +1,23 @@
 package ooga.engine.manager;
 
-public class CollisionManager {
+import java.util.List;
+import ooga.model.entity.EntityList;
+import ooga.utility.observer.Observable;
+import ooga.utility.observer.Observer;
 
+public class CollisionManager implements Observable {
+
+  private List<Observer> observers;
+
+  public void manageCollisions(EntityList entities) { }
+
+  @Override
+  public void addObserver(Observer o) {
+
+  }
+
+  @Override
+  public void notifyObservers() {
+
+  }
 }
