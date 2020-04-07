072b52db3200cd3d6ccbc9351f36ec85014bf066
CAYLA SCHUVAL <cas169@duke.edu>
2020-04-07 17:47:32 -0400

    created methods to communicate with the levelcontroller and started building the cameramanager

--- src/ooga/engine/loop/LevelLoop.java
+++ src/ooga/engine/loop/LevelLoop.java
@@ -3,71 +3,112 @@ package ooga.engine.loop;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
+import ooga.controller.Communicable;
+import ooga.controller.LevelController;
 import ooga.engine.manager.CameraManager;
 import ooga.engine.manager.CollisionManager;
 import ooga.engine.manager.EntityManager;
 import ooga.engine.manager.InputManager;
 import ooga.model.data.Level;
 import ooga.model.data.User;
+import ooga.model.entity.Entity;
 import ooga.model.entity.EntityList;
 
 public class LevelLoop implements Loopable {
 
+  private Communicable myLevelController;
   private EntityManager myEntityManager;
   private CameraManager myCameraManager;
   private InputManager myInputManager;
   private CollisionManager myCollisionManager;
   private EntityList myEntities;
+  private EntityList myVisibleEntities;
   private Level myLevel;
   private User myUser;
   private static final int FRAMES_PER_SECOND = 60;
   private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
   private Timeline myTimeline;
 
-  public LevelLoop() {
+  public LevelLoop(Communicable levelController, EntityList EntityList) {
+    myEntities = EntityList;
+    myLevelController = levelController;
+    myEntityManager = new EntityManager();
+    myCameraManager = new CameraManager(myEntities.getMainCharacter());
+    myInputManager = new InputManager();
+    myCollisionManager = new CollisionManager();
+    myCameraManager.activateEntities(myEntities);
+    //set initial entities
     KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> {
       loop();
     });
     myTimeline = new Timeline();
     myTimeline.setCycleCount(Timeline.INDEFINITE);
     myTimeline.getKeyFrames().add(frame);
-    begin();
-
-
   }
 
-  private void loop(){
+  private void loop() {
     processInput();
     manageCollisions();
     updateEntities();
+    // tell the entities to update gravity and stuff
     updateCamera();
   }
 
-  private void processInput() {}
-  private void manageCollisions(){}
-  private void updateEntities(){ }
-  private void updateCamera(){}
+  private void processInput() {
+  }
+
+  private void manageCollisions() {
+  }
+
+  private void updateEntities() {
+  }
+
+  private void updateCamera() {
+    EntityList activatedEntities = myCameraManager.getActivatedEntities();
+    EntityList deactivatedEntities = myCameraManager.getDeactivatedEntities();
+    sendEntitiesToController(activatedEntities, deactivatedEntities);
+
+  }
+
+  private void sendEntitiesToController(EntityList activatedEntities, EntityList deactivedEntities) {
+    /*for(Entity entity: activatedEntities){
+      sendrEntityToController(entity);
+  }
+    for(Entity entity: deactivedEntities){
+      removeEntityFromController(entity);
+    }*/
+  }
+
+  private void sendEntityToController(){
+  }
+
+  private void removeEntityFromController(){
+  }
+
 
   public void begin() {
     myTimeline.play();
-
   }
 
   public void end() {
     myTimeline.stop();
-
   }
 
-  public void pause(){
+  public void pause() {
     myTimeline.pause();
   }
 
   public void resume() {
     myTimeline.play();
-
   }
 
   public void exit() {
 
   }
+
+  public EntityList getIniitalVisibleEntityList() {
+  return myVisibleEntities;
+  }
+
+
 }
--- src/ooga/model/entity/EntityList.java
+++ src/ooga/model/entity/EntityList.java
@@ -1,77 +1,23 @@
 package ooga.model.entity;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import javafx.event.Event;
 import ooga.utility.observer.Observer;
 
-public class EntityList implements Iterable<Entity>, Collection<Entity>, Observer {
+public class EntityList implements Observer {
 
   private Collection<Entity> myEntities;
+  private Entity myMainCharacter;
 
-  @Override
-  public int size() {
-    return 0;
-  }
-
-  @Override
-  public boolean isEmpty() {
-    return false;
-  }
-
-  @Override
   public boolean contains(Object o) {
     return false;
   }
 
-  @Override
   public Iterator<Entity> iterator() {
     return null;
-  }
-
-  @Override
-  public Object[] toArray() {
-    return new Object[0];
-  }
-
-  @Override
-  public <T> T[] toArray(T[] a) {
-    return null;
-  }
-
-  @Override
-  public boolean add(Entity entity) {
-    return false;
-  }
-
-  @Override
-  public boolean remove(Object o) {
-    return false;
-  }
-
-  @Override
-  public boolean containsAll(Collection<?> c) {
-    return false;
-  }
-
-  @Override
-  public boolean addAll(Collection<? extends Entity> c) {
-    return false;
-  }
-
-  @Override
-  public boolean removeAll(Collection<?> c) {
-    return false;
-  }
-
-  @Override
-  public boolean retainAll(Collection<?> c) {
-    return false;
-  }
-
-  @Override
-  public void clear() {
-
   }
 
   @Override
@@ -83,5 +29,15 @@ public class EntityList implements Iterable<Entity>, Collection<Entity>, Observe
   }
 
   public void addEntity(Entity entityReceived) {
+  }
+
+  public Entity getMainCharacter() {
+
+    return myMainCharacter;
+  }
+
+  public List<Entity> getAsList(){
+    List<Entity> entityList = new ArrayList<Entity>();
+    return entityList;
   }
 }
