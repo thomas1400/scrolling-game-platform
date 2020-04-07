af8f26232de37fda07f3edce6a2400ac1ea86a7c
Dana Mulligan <dana.mulligan@duke.edu>
2020-04-06 21:58:41 -0400

    filling out health ability

--- src/ooga/model/ability/Health.java
+++ src/ooga/model/ability/Health.java
@@ -1,5 +1,43 @@
 package ooga.model.ability;
 
+/**
+ * Exists to keep track of the lives of an Entity. Handles being hit, and dying.
+ */
 public class Health extends Ability {
 
+  private static final int DEFAULT_LIVES = 1;
+
+  private int myLives;
+
+  /**
+   * Health constructor
+   * @param lives number to set myLives to
+   */
+  public Health(int lives){
+    myLives = lives;
+  }
+
+  /**
+   * Default Health constructor, gives 1 life
+   */
+  public Health(){
+    this(DEFAULT_LIVES);
+  }
+
+  /**
+   * Checks to see if the health is 'empty'
+   * @return if myLives is equal to zero
+   */
+  public boolean isDead(){
+    return myLives == 0;
+  }
+
+  /**
+   * Decreases the number of lives by one if the object is not dead
+   */
+  public void hit(){
+    if(!this.isDead()) {
+      myLives -= 1;
+    }
+  }
 }
