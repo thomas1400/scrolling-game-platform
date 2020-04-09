712d87139b201bcaaf33867c2c4d9dffa945e321
Thomas Owens <to57@duke.edu>
2020-04-09 16:12:09 -0400

    Added comment to CollisionEvent

--- src/ooga/utility/event/CollisionEvent.java
+++ src/ooga/utility/event/CollisionEvent.java
@@ -4,4 +4,28 @@ import ooga.model.entity.Entity;
 
 public class CollisionEvent extends Event {
 
+  /*
+  * Note (Thomas): this is all starter code that I just added for fun during plan! Feel free
+  * to remove/change any/all of this!
+   */
+
+
+  private Entity a, b;
+  private Side aSide, bSide;
+
+  public enum Side {
+    TOP,
+    BOTTOM,
+    SIDE
+  }
+
+  public CollisionEvent(Entity a, Side aSide, Entity b, Side bSide) { }
+
+  public Entity getOther(Entity one) {
+    if (a == one) {
+      return b;
+    } else {
+      return a;
+    }
+  }
 }
