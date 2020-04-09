48a6d39f9d0a951a4dc310d2cfff7edd2331301c
Dana Mulligan <dana.mulligan@duke.edu>
2020-04-09 19:24:28 -0400

    added an image to the entity

--- src/ooga/model/entity/Entity.java
+++ src/ooga/model/entity/Entity.java
@@ -1,10 +1,16 @@
 package ooga.model.entity;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import ooga.model.ability.Ability;
 import ooga.model.ability.Health;
+import ooga.model.ability.Movement;
+import ooga.model.ability.Stunnable;
+import ooga.model.ability.attacktypes.Attack;
 import ooga.model.behavior.Collidible;
 import ooga.model.behavior.CollisionBehaviorBundle;
 import ooga.model.physics.Physics;
@@ -14,26 +20,101 @@ public class Entity extends ImageView implements Collidible, Manageable, Rendera
 
   private CollisionBehaviorBundle cbb;
   private Physics myPhysics;
-  private Ability health;
+  private Health health;
+  private Movement movement;
+  private Ability stun;
+  private Attack side, top, bottom;
   private List<Ability> myAbilities;
-  private boolean visible;
+  private boolean dead;
 
-  public Entity(){
+  /**
+   * Create default health and attacks, which can be overwritten later
+   * using the addAbility method
+   */
+  public Entity(Image image){
+    super(image);
     myAbilities = new ArrayList<Ability>();
+    health = new Health();
+    side = Attack.HARMLESS;
+    top = Attack.BOUNCE;
+    bottom = Attack.DAMAGE;
+    stun = new Stunnable();
   }
 
-  public void addAbility(Ability a){
-    //TODO add a check here for if an ability of that type is already here. depending on how we do attacks then we might want to only allow one of each 'type'
-    if (a instanceof Health){
-      health = a;
-    } else {
-      myAbilities.add(a);
+  public void addAbility(String abilityType, Ability a){
+    //myAbilities.add(a);
+    //makeMethod("add"+abilityType);
+    try {
+      Method method = Entity.class.getDeclaredMethod("add"+abilityType);
+      method.invoke(Entity.this);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException(e);
+    } catch (InvocationTargetException e) {
+      throw new RuntimeException(e);
     }
+  }
+
+  private void addHealth(Health h){
+    health = h;
+  }
+
+  private void addSideAttack(Attack a){
+    side = a;
+  }
+
+  private void addTopAttack(Attack a){
+    top = a;
+  }
+
+  private void addBottomAttack(Attack a){
+    bottom = a;
   }
 
   @Override
   public String[] getTags() {
     return new String[0];
+  }
+
+  public boolean isDead(){
+    return dead;
+  }
+
+  /**
+   * Get the attack of the specific side
+   * @param location
+   * @return call method to get the attack of the specific location
+   */
+  public Attack getAttack(String location){
+    try {
+      Method method = Entity.class.getDeclaredMethod("get"+location+"Attack");
+      return (Attack) method.invoke(Entity.this);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException(e);
+    } catch (InvocationTargetException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  //used for reflection
+  private Attack getSideAttack(){
+    /*if(stun.isStunned()){
+      side = Attack.DAMAGE;
+    }*/
+    return side;
+  }
+
+  //used for reflection
+  private Attack getTopAttack(){
+    return top;
+  }
+
+  //used for reflection
+  private Attack getBottomAttack(){
+    return bottom;
   }
 
   @Override
@@ -58,9 +139,41 @@ public class Entity extends ImageView implements Collidible, Manageable, Rendera
      */
   }
 
+  //used for reflection
+  private void damage(){
+    health.hit();
+  }
+
+  /*
+  //used for reflection
+  private void stun(){
+    if(stunType.isStunnable()) {
+      stunType.setStunned(true);
+      side = Attack.HARMLESS;
+      top = Attack.BOUNCE;
+    } else {
+      damage();
+    }
+  }*/
+
+  //used for reflection
+  private void bounce(){
+    //if it's on the bottom
+    movement.jump();
+    //if it's side
+    /*if(stun.isStunned()){
+      side
+    }*/
+  }
+
+  //used for reflection
+  private void nothing(){
+    //do nothing
+  }
+
   @Override
   public void updateVisualization() {
-
+    //todo this is the moving thing?
   }
 
   public Renderable getRenderable() {
