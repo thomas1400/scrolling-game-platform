511075b39404a17c91c0434d3db7635ab00689ab
Thomas Owens <to57@duke.edu>
2020-04-09 16:10:11 -0400

    Removed old code from CollisionEvent

--- src/ooga/utility/event/CollisionEvent.java
+++ src/ooga/utility/event/CollisionEvent.java
@@ -4,23 +4,4 @@ import ooga.model.entity.Entity;
 
 public class CollisionEvent extends Event {
 
-  private Entity a, b;
-  private Side aSide, bSide;
-
-  public enum Side {
-    TOP,
-    BOTTOM,
-    SIDE
-  }
-
-  public CollisionEvent(Entity a, Side aSide, Entity b, Side bSide) { }
-
-  public Entity getOther(Entity one) {
-    if (a == one) {
-      return b;
-    } else {
-      return a;
-    }
-  }
-
 }
