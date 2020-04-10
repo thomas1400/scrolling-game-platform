fe82f5c314cf40ee271894db482b759ffd7bb1dd
CAYLA SCHUVAL <cas169@duke.edu>
2020-04-10 01:44:29 -0400

    added initial entities to entitylist

--- src/ooga/engine/manager/CameraManager.java
+++ src/ooga/engine/manager/CameraManager.java
@@ -16,13 +16,13 @@ public class CameraManager {
   public CameraManager(Entity character, double height, double width) {
     mainEntity = character;
     screenHeight = height;
-    screenWidth = width;
+    screenWidth = width +10;
     xCenter = screenWidth/2- mainEntity.getBoundsInLocal().getWidth()/2;
   }
 
   public void updateCamera(EntityList entities) {
     if (mainEntity.getX()>xCenter) {
-      double xChange = mainEntity.getX() - xCenter;
+      double xChange = mainEntity.getX()- xCenter;
       resetMainEntityToCenter();
       entities.changeAllXCoordinates(xChange);
       determineEntitiesOnScreen(entities);
@@ -42,7 +42,7 @@ public class CameraManager {
     initializeActivationStorage();
     onScreenEntities = new EntityList();
     for (Entity entity : entities) {
-      if (entity.getX() < screenWidth && entity.getY() < screenHeight) {
+      if (entity.getX()> 10 && entity.getBoundsInLocal().getMaxX()< screenWidth && entity.getY() < screenHeight) {
         activatedEntities.addEntity(entity);
         onScreenEntities.addEntity(entity);
       }
@@ -53,11 +53,11 @@ public class CameraManager {
   private void determineEntitiesOnScreen(EntityList entities) {
     initializeActivationStorage();
     for (Entity entity : entities) {
-      if (entity.getX() < screenWidth && entity.getY() < screenHeight && !onScreenEntities
+      if (entity.getX()> 10 && entity.getBoundsInLocal().getMaxX() < screenWidth && entity.getY() < screenHeight && !onScreenEntities
           .contains(entity)) {
         activatedEntities.addEntity(entity);
         onScreenEntities.addEntity(entity);
-      } else if ((entity.getX() > screenWidth || entity.getY() > screenHeight) && onScreenEntities
+      } else if ((entity.getBoundsInLocal().getMaxX() > screenWidth || entity.getX()< 10 || entity.getY() > screenHeight) && onScreenEntities
           .contains(entity)) {
         onScreenEntities.removeEntity(entity);
         deactivatedEntities.addEntity(entity);
