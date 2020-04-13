0553cfc5f193ea4ed29a17fa886e260d309cf095
CAYLA SCHUVAL <cas169@duke.edu>
2020-04-12 20:40:16 -0400

    merge issue

--- src/ooga/engine/manager/CameraManagers/StandardCameraManager.java
+++ src/ooga/engine/manager/CameraManagers/StandardCameraManager.java
@@ -32,3 +32,4 @@ public class StandardCameraManager extends CameraManager {
     mainEntity.setX(xCenter);
   }
 }
+//
--- src/ooga/engine/manager/CameraManagers/UpwardsCameraManager.java
+++ src/ooga/engine/manager/CameraManagers/UpwardsCameraManager.java
@@ -31,3 +31,4 @@ public class UpwardsCameraManager extends CameraManager {
     mainEntity.setY(yCenter);
   }
 }
+//
