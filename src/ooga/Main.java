24e98536686807865879537ee2807f6d6aa0aa2f
Grant LoPresti <gjl13@duke.edu>
2020-04-12 21:42:13 -0400

    fixed itgit add .git add .

--- src/ooga/Main.java
+++ src/ooga/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 import ooga.controller.OogaController;
-
+//OMG SO HAPPY MERGE IS FIXED!!
 public class Main extends Application {
 
     /**
