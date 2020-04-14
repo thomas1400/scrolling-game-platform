49af6da13e87ae413a639b4b919b5b236667d0c9
Grant LoPresti <gjl13@duke.edu>
2020-04-13 21:55:29 -0400

    level builder framework created

--- src/ooga/Tester.java
+++ src/ooga/Tester.java
@@ -1,15 +1,28 @@
 package ooga;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import ooga.controller.UserSaver;
-import ooga.controller.data.User;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ooga.controller.ScreenController;
+import ooga.controller.data.BasicLevelList;
+import ooga.controller.data.UserList;
+import ooga.view.LevelBuilderScreen;
 
-public class Tester {
+public class Tester extends Application {
 
-  public static void main(String[] args) throws FileNotFoundException {
-    ArrayList<Integer> unlocked = new ArrayList<>();
-    unlocked.add(7);
-    UserSaver.saveUser(new User("Thank You", "thankyou.png", unlocked, 56));
+  public static void main(String[] args){
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    ScreenController sc = new ScreenController(primaryStage, new UserList(), new BasicLevelList());
+
+    LevelBuilderScreen root = new LevelBuilderScreen(sc);
+
+    primaryStage.setScene(new Scene(root, 800, 600));
+    primaryStage.show();
   }
 }
+
