66404e5b7188c1d99c4dd712695e67ce78c8a810
Dana Mulligan <dana.mulligan@duke.edu>
2020-04-16 23:02:43 -0400

    updated debugging print statements to be more specific

--- src/ooga/engine/manager/CollisionManager.java
+++ src/ooga/engine/manager/CollisionManager.java
@@ -27,18 +27,18 @@ public class CollisionManager implements Observable {
             //check that the sides overlap, and that one entity 'box' envelops another.
             if (((entity.getBoundsInLocal().getMaxX() > entity2.getBoundsInLocal().getMinX()) || entity2.getBoundsInLocal().getMaxX() > entity.getBoundsInLocal().getMinX())
                 && (entity.getBoundsInLocal().getMinY() >= entity2.getBoundsInLocal().getMinY() && entity.getBoundsInLocal().getMaxY() <= entity2.getBoundsInLocal().getMaxY())){
-              //printDebug(SIDE, entity, entity2);
+              //printDebug(SIDE, SIDE, entity, entity2);
               createAndSendCollision(SIDE, entity2.getAttack(SIDE), entity);
               createAndSendCollision(SIDE, entity.getAttack(SIDE), entity2);
             } else if (entity.getBoundsInLocal().getMaxY() <= entity2.getBoundsInLocal()
                 .getMinY()) {
-              //printDebug(TOP, entity, entity2);
-              //printDebug(BOTTOM, entity2, entity);
+              //printDebug(TOP, BOTTOM, entity, entity2);
+              //printDebug(BOTTOM, TOP, entity2, entity);
               createAndSendCollision(TOP, entity2.getAttack(TOP), entity);
               createAndSendCollision(BOTTOM, entity.getAttack(BOTTOM), entity2);
             } else {
-              //printDebug(BOTTOM, entity, entity2);
-              //printDebug(TOP, entity2, entity);
+              //printDebug(BOTTOM, TOP, entity, entity2);
+              //printDebug(TOP, BOTTOM, entity2, entity);
               createAndSendCollision(BOTTOM, entity2.getAttack(BOTTOM), entity);
               createAndSendCollision(TOP, entity.getAttack(TOP), entity2);
             }
@@ -49,18 +49,19 @@ public class CollisionManager implements Observable {
   }
 
   //fixme delete after debugging over
-  private void printDebug(String loc, Entity a, Entity b){
+  private void printDebug(String aloc, String bloc, Entity a, Entity b){
     if((a.debug().equals("Mario.png")||b.debug().equals("Mario.png"))){// && (!a.debug().contains("Ground")&&!b.debug().contains("Ground"))) {
-      System.out.println("a max x: "+ a.getBoundsInLocal().getMaxX());
+      /*System.out.println("a max x: "+ a.getBoundsInLocal().getMaxX());
       System.out.println("a min x: " + a.getBoundsInLocal().getMinX());
       System.out.println("b max x: "+ b.getBoundsInLocal().getMaxX());
       System.out.println("b min x: " + b.getBoundsInLocal().getMinX());
       System.out.println("a max y: "+ a.getBoundsInLocal().getMaxY());
       System.out.println("a min y: " + a.getBoundsInLocal().getMinY());
       System.out.println("b max y: "+ b.getBoundsInLocal().getMaxY());
-      System.out.println("b min y: " + b.getBoundsInLocal().getMinY());
+      System.out.println("b min y: " + b.getBoundsInLocal().getMinY());*/
+      System.out.println("loc: "+aloc);
       System.out.println("Me: " + a.debug() + " && other:" + b.debug());
-      System.out.println("ATTACK TYPES: me: " + a.getAttack(loc) + " other: " + b.getAttack(loc));
+      System.out.println("ATTACK TYPES: me: " + a.getAttack(aloc) + " other: " + b.getAttack(bloc));
     }
   }
 
