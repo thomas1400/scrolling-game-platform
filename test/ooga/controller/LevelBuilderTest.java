d9af21fbfbec167744e71328acb79addc385997d
Grant LoPresti <gjl13@duke.edu>
2020-04-23 02:08:21 -0400

    beginnings of controller testing

---
+++ test/ooga/controller/LevelBuilderTest.java
@@ -0,0 +1,16 @@
+package ooga.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LevelBuilderTest {
+
+  @Test
+  void buildBasicLevel() {
+  }
+
+  @Test
+  void buildCompleteLevel() {
+  }
+}
---
+++ test/ooga/controller/users/UserTest.java
@@ -0,0 +1,68 @@
+package ooga.controller.users;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+  @Test
+  void saveUser() {
+  }
+
+  @Test
+  void asEntity() {
+  }
+
+  @Test
+  void setName() {
+  }
+
+  @Test
+  void setImage() {
+  }
+
+  @Test
+  void unlockNextLevel() {
+  }
+
+  @Test
+  void addLife() {
+  }
+
+  @Test
+  void adjustLives() {
+  }
+
+  @Test
+  void adjustPoints() {
+  }
+
+  @Test
+  void setPower() {
+  }
+
+  @Test
+  void setSize() {
+  }
+
+  @Test
+  void canConvertPointsToLife() {
+  }
+
+  @Test
+  void setGameLevels() {
+  }
+
+  @Test
+  void setAllGameLevels() {
+  }
+
+  @Test
+  void setLives() {
+  }
+
+  @Test
+  void addGame() {
+  }
+}
