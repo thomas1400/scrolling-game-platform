48b1c7c3c69484107f5bb13242c59221c74ac441
Grant LoPresti <gjl13@duke.edu>
2020-04-24 03:52:01 -0400

    beginnings of javadoc comments

--- src/ooga/Main.java
+++ src/ooga/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws FileNotFoundException {
+    public void start(Stage primaryStage) {
         new OogaController(primaryStage);
     }
 }
